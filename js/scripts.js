c0221e3560ae7bb046c60162eb6049c874cd3fbc
TK <tomaszkwiatkowski90@gmail.com>
2017-07-03 17:07:03 +0200

    commited

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+/*wyswietlanie alertu
+na stronie internetowej */
+
+
+// zmienna name
+var name = prompt('Enter your name');
+
+// wyœwietlenie alertu
+alert('Hello, ' + name);
+
+// consola
+console.log('Hello, ' + name);
